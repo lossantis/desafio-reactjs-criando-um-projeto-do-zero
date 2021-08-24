a3070718acb37ea19be30bd493679a613e9d4ab6
Hugo Leonardo da Silva Santos <hugo.desenvolvedor@gmail.com>
2021-08-24 17:02:18 -0300

    feat: get posts from prismic CMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
+import Prismic from '@prismicio/client';
+
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -24,13 +26,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(props: PostPagination) {
+  return (
+    <>
+    Home
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.query(
+    [
+        Prismic.predicates.at('document.type', 'post')
+    ],
+    {
+        fetch: [
+            'post.title',
+            'post.subtitle',
+            'post.author',
+        ],
+        pageSize: 100,
+    }
+  )
+
+  const results = response.results.map(post => {
+      return {
+          uid: post.uid,
+          first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+          }),
+          data: {
+            title: post.data.title,
+            subtitle: post.data.subtitle,
+            author: post.data.author,
+          },
+      }
+  })
+
+  return {
+    props: {
+      results,
+      next_page: ''
+    }
+  }
+};
